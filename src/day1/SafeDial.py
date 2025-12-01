ad3f3a835cc635896558d52c788a2842b665796e
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-01 19:58:53 +0100

    refactor: extract constant

--- src/day1/SafeDial.py
+++ src/day1/SafeDial.py
@@ -1,8 +1,10 @@
 class SafeDial:
+    SIZE_OF_WHEEL = 100
+
     position: int = 50
 
     def __init__(self):
         pass
 
     def rotate(self, amount: int):
-        self.position = (self.position + amount) % 100
+        self.position = (self.position + amount) % SafeDial.SIZE_OF_WHEEL
