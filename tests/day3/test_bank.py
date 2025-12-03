81762dbb814c562a871363cb03186d48a5c0dd99
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-03 21:25:22 +0100

    test: added tests for other sizes than 2

--- tests/day3/test_bank.py
+++ tests/day3/test_bank.py
@@ -14,6 +14,19 @@ def test_highest_joltage(bankstr, expected_result):
     bank = Bank(bankstr)
     assert bank.highest_joltage() == expected_result
 
+@pytest.mark.parametrize("bankstr, size, expected_result", [
+    ("987654321", 2, 98),
+    ("1", 1, 1),
+    ("11", 2, 11),
+    ("111", 3, 111),
+    ("2212", 3, 222),
+    ("987654321111111", 12, 987654321111),
+    ("234234234234278", 12, 434234234278)
+])
+def test_highest_joltage_with_size(bankstr, size, expected_result):
+    bank = Bank(bankstr)
+    assert bank.highest_joltage(size) == expected_result
+
 
 def test_get_sum_of_joltages():
     assert Bank.sum_of_joltages("12\n912\n001215") == 12+92+25
