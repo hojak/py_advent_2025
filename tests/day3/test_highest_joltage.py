830b7fc41d73e3fc2d8bc3add7bc21f1bf3a88da
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-03 18:07:28 +0100

    test: add more test samples

--- tests/day3/test_highest_joltage.py
+++ tests/day3/test_highest_joltage.py
@@ -7,6 +7,8 @@ from day3.highest_joltage import highest_joltage
     ("917654328", 98),
     ("917654320", 97),
     ("117654389", 89),
+    ("234234234234278", 78),
+    ("818181911112111", 92)
 ])
 def test_highest_joltage(bank, expected_result):
     assert highest_joltage(bank) == expected_result
