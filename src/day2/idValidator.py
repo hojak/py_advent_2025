8327891e13d7e5a1544df40eb7c005680026cad2
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-03 15:40:45 +0100

    refator: reimplemented is_valid on basis of deviding in 2 parts

--- src/day2/idValidator.py
+++ src/day2/idValidator.py
@@ -3,12 +3,22 @@ from math import floor
 
 class IdValidator:
 
+    def __init__(self):
+        pass
+
     def is_valid(self, some_id) -> bool:
         if (isinstance(some_id, int)):
             some_id = str(some_id)
 
-        half = floor(len(some_id) / 2)
-        return len(some_id) % 2 != 0 or some_id[:half] != some_id[half:]
+        return not self.is_repeated_pattern(some_id, 2)
+    
+    def is_repeated_pattern(self, some_id: str, number_of_parts: int) -> bool:
+        if len(some_id) % number_of_parts != 0:
+            return False
+        
+        part_length = floor(len(some_id) / number_of_parts)
+        possible_part = some_id[:part_length]
+        return some_id == possible_part * number_of_parts
 
     def get_invalid_ids(self, start: int, end: int) -> list:
         result = []
