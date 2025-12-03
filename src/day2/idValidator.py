65821b5a43ad98f50f2ce793df4b2d92507a41cf
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-03 01:06:01 +0100

    feat: accept ints as ids as well as strings

--- src/day2/idValidator.py
+++ src/day2/idValidator.py
@@ -3,6 +3,9 @@ from math import floor
 
 class IdValidator:  
 
-    def is_valid(some_id: str) -> bool:
+    def is_valid(some_id) -> bool:
+        if (isinstance(some_id, int)):
+            some_id = str(some_id)
+            
         half = floor(len(some_id) / 2)
         return len(some_id) % 2 != 0 or some_id[:half] != some_id[half:]
--- tests/day2/test_id_validator.py
+++ tests/day2/test_id_validator.py
@@ -14,10 +14,14 @@ def test_is_valid_is_true(id):
 
 
 @pytest.mark.parametrize("id", [
-    "11",
+    11,
     "1010",
     "10101010",
     "123123"
 ])
 def test_is_valid_is_false(id):
     assert not IdValidator.is_valid(id)
+    
+    
+def test_get_invalid_ids_from_range():
+    pass
