9cf9e738ed14937e249dc116a0216ea8f75b2e7f
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-03 20:47:53 +0100

    refactor: restructuring hightest_voltage
    with aim to get number of selected batteries as parameter

--- src/day3/bank.py
+++ src/day3/bank.py
@@ -1,7 +1,7 @@
 class Bank:
     
     def __init__(self, bankstr: str):
-        self.bankstr = bankstr
+        self.bankstr = "0"+bankstr
 
     def len(self) -> int:
         return len(self.bankstr)
@@ -9,23 +9,30 @@ class Bank:
     def battery(self, index: int) -> int:
         return int(self.bankstr[index])
 
-    def highest_joltage(self) -> int:
-        largest_first = 0
-        index_of_first = 0
+    def highest_joltage(self, size=2) -> int:
+        indices = [0, self.len()-1, self.len()]
 
-        for first_index in range(0, self.len()-1):
-            int_at_first = self.battery(first_index)
-            if (int_at_first > largest_first):
-                largest_first = int_at_first
-                index_of_first = first_index
+        largest_found = 0
+        for index in range(indices[0], indices[1]):
+            found_int = self.battery(index)
+            if (found_int > largest_found):
+                largest_found = found_int
+                indices[1] = index
 
-        largest_second = 0
-        for second_index in range(index_of_first+1, self.len()):
-            int_at_second = self.battery(second_index)
-            if (int_at_second > largest_second):
-                largest_second = int_at_second
+        largest_found = 0
+        for index in range(indices[1]+1, indices[2]):
+            found_int = self.battery(index)
+            if (found_int > largest_found):
+                largest_found = found_int
+                indices[2] = index
 
-        return largest_first * 10 + largest_second
+        return self.get_power(indices)
+    
+    def get_power(self, indices: list[int]) -> int:
+        str = ""
+        for index in indices:
+            str += self.bankstr[index]
+        return int(str)
 
     def sum_of_joltages(banks: str) -> int:
         result = 0
