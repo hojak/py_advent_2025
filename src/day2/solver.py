c4b5cd4067de3acb616ab49d30ff32b8ecf8100d
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-03 16:55:44 +0100

    feat: implemented solver for part 2

--- src/day2/solver.py
+++ src/day2/solver.py
@@ -8,3 +8,8 @@ class Solver(ProblemSolver):
         validator = IdValidator()
         result = validator.get_checksum(self.parsed_input)
         print("result: ", result)
+
+    def solve_part2(self):
+        validator = IdValidator([2, 3, 4, 5, 6, 7, 8, 9, 10])
+        result = validator.get_checksum(self.parsed_input)
+        print("result: ", result)
