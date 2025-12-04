f937d364f6036429fc3043237489897201a64acb
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-04 14:32:38 +0100

    fix: corrected test map

--- src/day4/paperWarehouse.py
+++ src/day4/paperWarehouse.py
@@ -3,4 +3,4 @@ from adv25Tools.stringMap import StringMap
 
 class PaperWarehouse(StringMap):
     def __init__(self, layout: str):
-        super.__init__(layout)
+        super().__init__(layout)
--- tests/day4/test_paper_warehouse.py
+++ tests/day4/test_paper_warehouse.py
@@ -2,6 +2,6 @@ from day4.paperWarehouse import PaperWarehouse
 
 
 def test_create_warehouse():
-    testee = PaperWarehouse("@..@\n...\n@@@@")
+    testee = PaperWarehouse("@..@\n....\n@@@@")
     assert testee.width == 4
     assert testee.height == 3
