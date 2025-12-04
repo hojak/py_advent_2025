f1f54566f59174ed855011b68c92da82c4100c31
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-04 14:35:02 +0100

    feat: warehous can check, if a space is_occupied

--- src/day4/paperWarehouse.py
+++ src/day4/paperWarehouse.py
@@ -1,6 +1,10 @@
+from adv25Tools.coordinates import Coordinates
 from adv25Tools.stringMap import StringMap
 
 
 class PaperWarehouse(StringMap):
     def __init__(self, layout: str):
         super().__init__(layout)
+
+    def is_occupied(self, x: int, y: int) -> bool:
+        return self.get_char_at(Coordinates(x, y)) == '@'
--- tests/day4/test_paper_warehouse.py
+++ tests/day4/test_paper_warehouse.py
@@ -5,3 +5,22 @@ def test_create_warehouse():
     testee = PaperWarehouse("@..@\n....\n@@@@")
     assert testee.width == 4
     assert testee.height == 3
+
+
+def test_is_occupied():
+    testee = PaperWarehouse("@..@\n....\n@@@@")
+    assert testee.is_occupied(0, 0) is True
+    assert testee.is_occupied(1, 0) is False
+    assert testee.is_occupied(2, 0) is False
+    assert testee.is_occupied(3, 0) is True
+
+    assert testee.is_occupied(0, 1) is False
+    assert testee.is_occupied(1, 1) is False
+    assert testee.is_occupied(2, 1) is False
+    assert testee.is_occupied(3, 1) is False
+
+    assert testee.is_occupied(0, 2) is True
+    assert testee.is_occupied(1, 2) is True
+    assert testee.is_occupied(2, 2) is True
+    assert testee.is_occupied(3, 2) is True
+
