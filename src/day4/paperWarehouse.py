f85d975bf54b3dd21b7ba9286a72b179ec92a7f5
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-04 14:21:49 +0100

    feat: implemented paper warehouse as stringmap

---
+++ src/day4/paperWarehouse.py
@@ -0,0 +1,6 @@
+from adv25Tools.stringMap import StringMap
+
+
+class PaperWarehouse(StringMap):
+    def __init__(self, layout: str):
+        super.__init__(layout)
---
+++ tests/day4/test_paperWarehouse.py
@@ -0,0 +1,7 @@
+from day4.paperWarehouse import PaperWarehouse
+
+
+def test_create_warehouse():
+    testee = PaperWarehouse("@..@\n...\n@@@@")
+    assert testee.width == 4
+    assert testee.height == 3
