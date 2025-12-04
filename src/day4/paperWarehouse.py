4abbd0dded50ece2d802758123c06aba70eedcdd
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-04 15:05:27 +0100

    feat: warehous can decide, if a roll is_movable

--- src/day4/paperWarehouse.py
+++ src/day4/paperWarehouse.py
@@ -8,3 +8,29 @@ class PaperWarehouse(StringMap):
 
     def is_occupied(self, x: int, y: int) -> bool:
         return self.get_char_at(Coordinates(x, y)) == '@'
+
+    def is_movable(self, x: int, y: int) -> bool:
+        if (not self.is_occupied(x, y)):
+            return False
+     
+        occupied_neighbors = 0
+        for location in self.get_neighbor_coordinates(x, y):
+            if self.is_occupied(location.x, location.y):
+                occupied_neighbors += 1
+
+        return occupied_neighbors <= 4
+    
+    def get_neighbor_coordinates(self, x: int, y: int) -> list[Coordinates]:
+        neighbors = []
+        directions = [
+            Coordinates(-1, -1), Coordinates(0, -1), Coordinates(1, -1),
+            Coordinates(-1, 0), Coordinates(1, 0),
+            Coordinates(-1, 1), Coordinates(0, 1), Coordinates(1, 1),
+        ]
+
+        for direction in directions:
+            position = direction.add(Coordinates(x, y))
+            if self.is_within_bounds(position):
+                neighbors.append(position)
+
+        return neighbors
--- tests/day4/test_paper_warehouse.py
+++ tests/day4/test_paper_warehouse.py
@@ -1,3 +1,4 @@
+import pytest
 from day4.paperWarehouse import PaperWarehouse
 
 
@@ -24,3 +25,15 @@ def test_is_occupied():
     assert testee.is_occupied(2, 2) is True
     assert testee.is_occupied(3, 2) is True
 
+
+@pytest.mark.parametrize("layout, x, y, expected", [
+    ("@@.\n@.@\n...", 1, 1, False),
+    ("@@.\n@@@\n...", 1, 1, True),
+    ("@@.\n@@@\n.@.", 1, 1, False),
+    ("@@.\n@@@\n.@.", 0, 0, True),
+    ("@@@\n@@@\n.@.", 1, 0, False),
+
+])
+def test_is_movable(layout, x, y, expected):
+    testee = PaperWarehouse(layout)
+    assert testee.is_movable(x, y) is expected
