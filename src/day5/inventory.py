27761c403e10894b05de848c0b80b6152b30312d
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-05 11:05:10 +0100

    feat: inventory can handle one range of fresh ids

--- src/day5/inventory.py
+++ src/day5/inventory.py
@@ -1,4 +1,16 @@
 class Inventory:
 
+    fresh_ranges = []
+
+    def is_in_range(id: int, range: tuple) -> bool:
+        return range[0] <= id <= range[1]
+
     def is_fresh(self, id: int) -> bool:
+        for range in self.fresh_ranges:
+            if Inventory.is_in_range(id, range):
+                return True
+
         return False
+
+    def add_fresh_id_range(self, start_id: int, end_id: int) -> None:
+        self.fresh_ranges.append((start_id, end_id))
--- tests/day5/test_inventory.py
+++ tests/day5/test_inventory.py
@@ -4,3 +4,15 @@ from day5.inventory import Inventory
 def test_nothing_fresh_in_empty_inventory():
     inventory = Inventory()
     assert inventory.is_fresh(10) is False
+
+
+def test_adding_one_fresh_id_range():
+    inventory = Inventory()
+    inventory.add_fresh_id_range(5, 7)
+
+    assert inventory.is_fresh(4) is False
+    assert inventory.is_fresh(8) is False
+
+    assert inventory.is_fresh(5) is True
+    assert inventory.is_fresh(6) is True
+    assert inventory.is_fresh(7) is True
