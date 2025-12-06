ff0c963807553e6203f452175570e0784aebab7d
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-06 12:18:50 +0100

    feat: worksheet can sum up one row

--- src/day6/worksheet.py
+++ src/day6/worksheet.py
@@ -1,4 +1,29 @@
 class Worksheet:
-    
+
+    def __init__(self):
+        self.number_sheet = []
+
     def get_grand_total(self) -> int:
-        return 0
+        column_results = []
+
+        for index in range(0, len(self.number_sheet)):
+            column_results.append(self.column_result(index))
+
+        return sum(column_results)
+
+    def column_result(self, index: int) -> int:
+        return sum(self.number_sheet[index])
+
+    def add_line(self, line: str):
+        numbers_in_line = line.split(' ')
+
+        if (len(self.number_sheet) == 0):
+            self.init_numbers(len(line))
+
+        for index in range(0, len(numbers_in_line)):
+            self.number_sheet[index].append(int(numbers_in_line[index]))
+
+    def init_numbers(self, length):
+        self.number_sheet = []
+        for i in range(0, length):
+            self.number_sheet.append([])
--- tests/day6/test_worksheet.py
+++ tests/day6/test_worksheet.py
@@ -5,3 +5,10 @@ def test_empty_worksheet():
     worksheet = Worksheet()
 
     assert worksheet.get_grand_total() == 0
+
+
+def test_add_line():
+    worksheet = Worksheet()
+    worksheet.add_line("1 1 1 1")
+
+    assert worksheet.get_grand_total() == 4
