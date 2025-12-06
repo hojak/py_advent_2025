10ac474232c86c6f587013977f6a564473341373
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-06 12:19:40 +0100

    test: adding up multiple rows works as well

--- tests/day6/test_worksheet.py
+++ tests/day6/test_worksheet.py
@@ -12,3 +12,11 @@ def test_add_line():
     worksheet.add_line("1 1 1 1")
 
     assert worksheet.get_grand_total() == 4
+
+
+def test_add_lines():
+    worksheet = Worksheet()
+    worksheet.add_line("1 1 1 1")
+    worksheet.add_line("2 2 2 2")
+
+    assert worksheet.get_grand_total() == 12
