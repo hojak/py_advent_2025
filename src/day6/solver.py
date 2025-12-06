a30dee98b1b9279d23f3dd123af07c638aff1ebd
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-06 12:52:31 +0100

    feat: implement solver for part 1

---
+++ src/day6/solver.py
@@ -0,0 +1,15 @@
+from adv25Tools.problemSolver import ProblemSolver
+from day6.worksheet import Worksheet
+
+
+class Solver(ProblemSolver):
+    def solve_part1(self):
+        worksheet = Worksheet()
+
+        for line in self.parsed_input.splitlines():
+            if line.count('+') + line.count('*') == 0:
+                worksheet.add_line(line)
+            else:
+                worksheet.set_operants(line)
+
+        print("summing up: ", worksheet.get_grand_total())
