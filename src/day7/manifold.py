27d50c721da1ee473dd7dbc73ae73bf41841288e
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-08 12:51:09 +0100

    feat: beam starts beaming

--- src/day7/manifold.py
+++ src/day7/manifold.py
@@ -1,13 +1,34 @@
 class Manifold:
 
     def __init__(self, layout: str):
-        self.initial_layout = layout
+        self.initial_layout = layout.splitlines()
 
     def fire_beam(self):
-        self.beam_layout = self.initial_layout
+        self.beam_layout = [self.initial_layout[0]]
+
+        for line_index in range(1, len(self.initial_layout)):
+            initial_line = self.initial_layout[line_index]
+            previous_line = self.beam_layout[line_index - 1]
+
+            beamed_line = self.continue_beams(previous_line, initial_line)
+
+            self.beam_layout.append(beamed_line)
+
+    def continue_beams(self, previous_line: str, layout_of_current_line: str) -> str:
+        result = ""
+        for position in range(0, len(layout_of_current_line)):
+            if layout_of_current_line[position] == "." and (
+                    previous_line[position] == "S"
+                    or previous_line[position] == "|"
+                    ):
+                result += "|"
+            else:
+                result += layout_of_current_line[position]
+
+        return result
 
     def get_beam_layout(self) -> str:
-        return self.beam_layout
-    
+        return "\n".join(self.beam_layout)
+
     def get_number_of_splitted_beams(self) -> int:
         return 0
--- tests/day7/test_manifold.py
+++ tests/day7/test_manifold.py
@@ -7,3 +7,19 @@ def test_simple_beam():
 
     assert manifold.get_beam_layout() == ".S."
     assert manifold.get_number_of_splitted_beams() == 0
+
+
+def test_beam_expands_straight_forward_from_start():
+    manifold = Manifold(".S.\n...")
+    manifold.fire_beam()
+
+    assert manifold.get_beam_layout() == ".S.\n.|."
+    assert manifold.get_number_of_splitted_beams() == 0
+
+
+def test_beam_expands_straight_forward():
+    manifold = Manifold(".S.\n...\n...\n...")
+    manifold.fire_beam()
+
+    assert manifold.get_beam_layout() == ".S.\n.|.\n.|.\n.|."
+    assert manifold.get_number_of_splitted_beams() == 0
