28fed3dfd11893f9dabee93803255b3906649c54
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-08 01:07:53 +0100

    feat: math also can multiply

--- src/day6/cephalopods_math.py
+++ src/day6/cephalopods_math.py
@@ -8,9 +8,26 @@ class CephalopodsMath:
 
     def get_result(self):
         total_result = 0
+        multiply = False
 
+        sub_result = 0
         for line in self.transposed_content:
-            number_text = line[0:-1].strip()
-            total_result += int(number_text)
+            if (line.strip() == ""):
+                total_result += sub_result
+                sub_result = 0
+                multiply = False
+            else:
+                if (line[-1] == "*"):
+                    multiply = True
+                    sub_result = 1
+
+                number_text = line[0:-1].strip()
+
+                if (multiply):
+                    sub_result *= int(number_text)
+                else:
+                    sub_result += int(number_text)
+
+        total_result += sub_result
 
         return total_result
--- tests/day6/test_cephalopods_math.py
+++ tests/day6/test_cephalopods_math.py
@@ -5,3 +5,9 @@ def test_simple_sum():
     testee = CephalopodsMath("12\n12\n+ ")
 
     assert testee.get_result() == 33
+
+
+def test_sum_and_multiply():
+    testee = CephalopodsMath("12 2  \n12  2 \n+  *  ")
+
+    assert testee.get_result() == 37
