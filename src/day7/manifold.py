818a5d58374d3e0743c1ba3fcb7129edcae842a1
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-08 14:45:31 +0100

    feat: beam gets splitted

--- src/day7/manifold.py
+++ src/day7/manifold.py
@@ -9,8 +9,8 @@ class Manifold:
         for line_index in range(1, len(self.initial_layout)):
             initial_line = self.initial_layout[line_index]
             previous_line = self.beam_layout[line_index - 1]
-
             beamed_line = self.continue_beams(previous_line, initial_line)
+            beamed_line = self.split_beams(previous_line, beamed_line)
 
             self.beam_layout.append(beamed_line)
 
@@ -26,9 +26,27 @@ class Manifold:
                 result += layout_of_current_line[position]
 
         return result
+    
+    def split_beams(self, previous_line: str, beamed_line: str) -> str:
+        result = beamed_line
+
+        for index in range(0, len(beamed_line)):
+            if beamed_line[index] == "^" and previous_line[index] == "|":
+                result = Manifold.replace_char_at(result, index, "v")
+
+                if index > 0 and result[index-1] == ".":
+                    result = Manifold.replace_char_at(result, index-1, "|")
+                
+                if index < len(beamed_line) - 1 and result[index+1] == ".":
+                    result = Manifold.replace_char_at(result, index+1, "|")
+
+        return result
 
     def get_beam_layout(self) -> str:
         return "\n".join(self.beam_layout)
 
     def get_number_of_splitted_beams(self) -> int:
-        return 0
+        return "".join(self.beam_layout).count("v")
+
+    def replace_char_at(string: str, index: int, new_char: str) -> str:
+        return string[:index] + new_char + string[index + 1:]
