55e2cd93c896612e7d25038df47468d62b11dd55
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-10 18:07:34 +0100

    feat: tile floor returns size of biggest rectangle

--- tests/day9/test_tile_floor.py
+++ tests/day9/test_tile_floor.py
@@ -6,3 +6,20 @@ def test_add_tile():
     floor = TileFloor()
     floor.add_tile(Position(2, 3))
     assert len(floor.tiles) == 1
+
+
+def test_get_size_of_biggest_rectangle_of_one():
+    floor = TileFloor()
+    floor.add_tile(Position(1, 1))
+    floor.add_tile(Position(3, 4))
+
+    assert floor.get_size_of_bissgest_rectangle() == 6
+
+
+def test_get_size_of_biggest_rectangle_of_one():
+    floor = TileFloor()
+    floor.add_tile(Position(2, 2))
+    floor.add_tile(Position(1, 1))
+    floor.add_tile(Position(3, 4))
+
+    assert floor.get_size_of_bissgest_rectangle() == 6
