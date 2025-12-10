b2ab02022ad4045907c0b25fafb3a89ca28762ec
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-10 01:45:19 +0100

    feat: junctions boxes can state their distance

--- src/day8/junctionBox.py
+++ src/day8/junctionBox.py
@@ -1,6 +1,16 @@
+from math import sqrt
+
+
 class JunctionBox:
     
     def __init__(self, x: int, y: int, z: int):
         self.x = x
         self.y = y
         self.z = z
+        
+    def distance_to(self, other: object) -> float:
+        return sqrt(
+            (self.x - other.x) ** 2
+            + (self.y-other.y) ** 2
+            + (self.z - other.z) ** 2
+        )
--- tests/day8/test_junction_box.py
+++ tests/day8/test_junction_box.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from day8.junctionBox import JunctionBox
 
 
@@ -6,4 +7,10 @@ def test_create():
     assert box.x == 1
     assert box.y == 2
     assert box.z == 3
+
+
+def test_distance():
+    box_a = JunctionBox(0, 0, 0)
+    box_b = JunctionBox(10, 10, 10)
     
+    assert box_a.distance_to(box_b) == box_b.distance_to(box_a) == sqrt(300)
