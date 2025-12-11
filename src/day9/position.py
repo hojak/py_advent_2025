0341ce65331ec22f2c7b45c810cdf629219541ee
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-11 10:14:57 +0100

    feat: positions are sortable

--- src/day9/position.py
+++ src/day9/position.py
@@ -8,3 +8,30 @@ class Position:
         width = abs(self.x - other.x) + 1
         height = abs(self.y - other.y) + 1
         return width * height
+
+    def __lt__(self, other: 'Position') -> bool:
+        if (self.y == other.y):
+            return self.x < other.x
+        else:
+            return self.y < other.y
+
+    def __gt__(self, other: 'Position'):
+        if (self.y == other.y):
+            return self.x > other.x
+        else:
+            return self.y > other.y
+
+    def __eq__(self, other: 'Position'):
+        return self.x == other.x and self.y == other.y
+
+    def __le__(self, other: 'Position'):
+        return not self.__gt__(other)
+
+    def __ge__(self, other: 'Position'):
+        return not self.__lt__(other)
+
+    def __ne__(self, other: 'Position'):
+        return not self.__eq__(other)
+    
+    def __str__(self):
+        return f"({self.x}, {self.y})"
--- tests/day9/test_position.py
+++ tests/day9/test_position.py
@@ -6,3 +6,28 @@ def test_rectangle():
     position_2 = Position(3, 4)
 
     assert position_1.size_of_rectangle_with(position_2) == 12
+
+
+def test_positions_are_sortable():
+    position_1 = Position(x=1, y=4)
+    position_2 = Position(1, 1)
+    position_3 = Position(2, 4)
+    position_4 = Position(2, 1)
+
+    list = [position_1, position_2, position_3, position_4]
+
+    list.sort()
+
+    assert list == [position_2, position_4, position_1, position_3]
+
+
+def test_greater_than():
+    position_1 = Position(1, 4)
+    position_2 = Position(1, 1)
+    position_3 = Position(2, 4)
+    position_4 = Position(2, 1)
+
+    assert position_1 > position_2
+    assert position_3 > position_1
+    assert position_3 > position_2
+    assert position_4 > position_2
