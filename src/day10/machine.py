992c55c3f7a6b6c57886b639f0100c416777976c
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-12 01:34:47 +0100

    feat: initialize correct number of buttons

--- src/day10/machine.py
+++ src/day10/machine.py
@@ -7,7 +7,13 @@ class Machine:
     def __init__(self, init_str: str):
         parts = init_str.split(" ")
         self.indicator_lights = IndicatorLights(parts[0][1:-1])
+        
+        self.init_buttons(parts[1:-1])
+
+    def init_buttons(self, button_definitions: list[str]):
         self.buttons = []
+        for definition in button_definitions:
+            self.buttons.append(Button(definition[1:-1]))
 
     def get_indicator_lights(self) -> 'IndicatorLights':
         return self.indicator_lights
--- tests/day10/test_machine.py
+++ tests/day10/test_machine.py
@@ -6,10 +6,10 @@ def test_init_from_string():
 
     assert machine is not None
     assert machine.get_indicator_lights().number_of_lights() == 4
-    assert machine.get_buttons() is not None
+    assert len(machine.get_buttons()) == 6
 
 
 def test_number_of_lights():
-    machine = Machine("[.##.#.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}")
+    machine = Machine("[.##.#.] (3) (1,3) (2) (2,3) (0,1) {3,5,4,7}")
 
     assert machine.get_indicator_lights().number_of_lights() == 6
