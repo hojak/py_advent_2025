2142e64d11a20ec8b49dbba3280094e1ebde8f4f
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2025-12-12 08:25:31 +0100

    feat: we can press a button on an indicator lighs

--- src/day10/indicatorLights.py
+++ src/day10/indicatorLights.py
@@ -1,10 +1,22 @@
+from day7.manifold import Manifold
+
+
 class IndicatorLights:
 
     def __init__(self, init_string: str):
-        self.lights_are_on = []
-
-        for index in range(0, len(init_string)):
-            self.lights_are_on.append(False)
+        self.status = init_string
 
     def number_of_lights(self) -> int:
-        return len(self.lights_are_on)
+        return len(self.status)
+
+    def push_button(self, button) -> 'IndicatorLights':
+        new_status = self.status
+        for switch_light in button.connected_lights:
+            new_light_status = '#' if new_status[switch_light] == '.' else '.'
+            new_status = Manifold.replace_char_at(
+                new_status, switch_light, new_light_status)
+
+        return IndicatorLights(new_status)
+
+    def __str__(self) -> str:
+        return self.status
