f1ffb642ee1ccfc3e0db723ae93e500153918109
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-13 12:17:09 +0100

    feat: implement solver for part 2
    in general it should be working, but takes way too much time

--- src/day10/solver.py
+++ src/day10/solver.py
@@ -12,5 +12,16 @@ class Solver(ProblemSolver):
             minimal_sequence = machine.get_minimal_buttons_for_start()
             result += len(minimal_sequence)
             print("  found sequence:", minimal_sequence)
-        
+
+        print("Total minimal buttons to press:", result)
+
+    def solve_part2(self):
+        result = 0
+        for line in self.parsed_input.splitlines():
+            print("Processing line:", line)
+            machine = Machine(line)
+            minimal_sequence = machine.get_minimal_buttons_for_joltage()
+            result += len(minimal_sequence)
+            print("  found sequence:", minimal_sequence)
+
         print("Total minimal buttons to press:", result)
