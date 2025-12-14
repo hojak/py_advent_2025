c1bb6128be072d83d542485ead24fe37949a4e75
Holger Janßen-Kroll <github@ho-jk.de>
2025-12-14 12:21:15 +0100

    feat: find paths in network

--- src/day11/network.py
+++ src/day11/network.py
@@ -9,15 +9,35 @@ class Network:
 
             self.add_node(source_node)
 
-            for target_node in connected_to.strip().split(","):
+            for target_node in connected_to.strip().split(" "):
                 target_node = target_node.strip()
                 self.add_node(source_node.strip())
 
                 self.nodes[source_node].append(target_node)
 
     def size(self) -> int:
-        return len(self.nodes)            
+        return len(self.nodes)
 
     def add_node(self, node_name: str):
         if node_name not in self.nodes:
             self.nodes[node_name] = []
+
+    def find_all_paths_from_to(self, start: str, goal: str) -> list[list[str]]:
+        result = []
+        queue = [(start, [start])]
+
+        while queue:
+            (current_node, path) = queue.pop(0)
+
+            if current_node == goal:
+                result.append(path)
+                continue
+
+            for neighbor in self.nodes[current_node]:
+                if neighbor not in path:
+                    queue.append((neighbor, path + [neighbor]))
+
+        return result
+
+    def find_number_of_possible_paths(self, start: str, goal: str) -> int:
+        return len(self.find_all_paths_from_to(start, goal))
--- tests/day11/test_network.py
+++ tests/day11/test_network.py
@@ -4,3 +4,24 @@ from day11.network import Network
 def test_init_network():
     network = Network("you: out")
     assert network.size() == 1
+
+
+def test_find_the_only_path():
+    network = Network("you: aaa\naaa: ccc\nbbb: aaa out\nccc: out")
+    assert network.find_all_paths_from_to("you", "out") \
+        == [["you", "aaa", "ccc", "out"]]
+
+
+def test_find_number_of_possible_paths():
+    network = Network('''aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out''')
+    
+    assert network.find_number_of_possible_paths("you", "out") == 5
